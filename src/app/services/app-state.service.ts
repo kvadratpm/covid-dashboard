e6aab3592651e73c37948c3ad1e90fbc167b8d2d
Maksim Perevyazkin <kvadratpm@gmail.com>
2020-12-23 21:52:49 +0300

    feat: add app state

--- src/app/services/app-state.service.ts
+++ src/app/services/app-state.service.ts
@@ -1,9 +1,38 @@
+import { state } from '@angular/animations';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppStateService {
 
-  constructor() { }
+  stats: Array<string> = [
+    'TotalConfirmed',
+    'NewConfirmed',
+    'NewDeaths',
+    'TotalDeaths',
+    'NewRecovered',
+    'TotalRecovered',
+  ];
+
+  statNum = 0;
+
+  currentStat!: string;
+
+
+  constructor() {
+    this.currentStat = this.stats[this.statNum];
+  }
+
+  upState(): void {
+    this.statNum = this.statNum === 5 ? 0 : this.statNum += 1;
+    this.currentStat = this.stats[this.statNum];
+  }
+
+  downState(): void {
+    this.statNum = this.statNum === 5 ? 0 : this.statNum += 1;
+    this.currentStat = this.stats[this.statNum];
+  }
+
 }
